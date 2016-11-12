2bb3d9239b4b348153e31397b68a9d6f860b4cb4
idefav <wzs2009_hi@126.com>
2016-11-12 22:13:10 +0800

    35

--- CrawlServices/Program.cs
+++ CrawlServices/Program.cs
@@ -11,39 +11,39 @@ namespace CrawlServices
     {
         static void Main(string[] args)
         {
-            //TaskManager mgr = new TaskManager();
-            //mgr.Init();
-            //mgr.Run(new CancellationToken());
-            //Console.ReadLine();
+            TaskManager mgr = new TaskManager();
+            mgr.Init();
+            mgr.Run(new CancellationToken());
+            Console.ReadLine();
 
 
 
-            string server = "抓取服务";
-            string description = "抓取服务";
+            //string server = "抓取服务";
+            //string description = "抓取服务";
 
-            if (args.Length >= 2)
-            {
-                server = args[1];
-                InstallService iS = new InstallService();
-                if (args[0] == "add")
-                {
-                    if (args.Length == 3)
-                        description = args[2];
-                    var result = iS.Install(System.Reflection.Assembly.GetExecutingAssembly().Location, server, server, description);
-                }
-                if (args[0] == "del")
-                    iS.UnInstall(server);
+            //if (args.Length >= 2)
+            //{
+            //    server = args[1];
+            //    InstallService iS = new InstallService();
+            //    if (args[0] == "add")
+            //    {
+            //        if (args.Length == 3)
+            //            description = args[2];
+            //        var result = iS.Install(System.Reflection.Assembly.GetExecutingAssembly().Location, server, server, description);
+            //    }
+            //    if (args[0] == "del")
+            //        iS.UnInstall(server);
 
-            }
-            else
-            {
-                ServiceBase[] ServicesToRun;
-                ServicesToRun = new ServiceBase[]
-            {
-                    new CrawlService(),
-            };
-                ServiceBase.Run(ServicesToRun);
-            }
+            //}
+            //else
+            //{
+            //    ServiceBase[] ServicesToRun;
+            //    ServicesToRun = new ServiceBase[]
+            //{
+            //        new CrawlService(),
+            //};
+            //    ServiceBase.Run(ServicesToRun);
+            //}
         }
     }
 }
